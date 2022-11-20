f2fdf53c00f9488efe4a1ba814a25a91849376e6
ViktorUserName <vityachishevich@gmail.com>
2022-11-20 23:55:46 +0300

    up

--- front/src/components/menu/Menu.tsx
+++ front/src/components/menu/Menu.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import s from './Menu.module.scss'
+import {Link } from "react-router-dom";
+import logo from '../../assets/mainPage/pixema.svg'
 
 const Menu: React.FC = () => {
     return (
         <div className={s.menu}>
             <div className={s.menuContent}>
-                
+                <Link to='/'><img src={logo} alt="" /></Link>
+                <div className={s.menuOptions}>
+                    <Link to='/'>
+                        <p>Home</p>
+                    </Link>
+                    <Link>
+                        <p>Trends</p>
+                    </Link>
+                    <Link>
+                        <p>Favorites</p>
+                    </Link>
+                    <Link>
+                        <p>Settings</p>
+                    </Link>
+                </div>
             </div>
         </div>
     );
